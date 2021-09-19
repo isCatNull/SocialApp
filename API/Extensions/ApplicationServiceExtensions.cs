033590cc117d9d03fadf90f367261d1aba80d66d
Ieva-Erika Janovic <ierikajanovic@gmail.com>
2021-09-19 22:04:51 +0300

    Clean up

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -9,7 +9,8 @@ namespace API.Extensions
 {
     public static class ApplicationServiceExtensions
     {
-        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        public static IServiceCollection AddApplicationServices(
+            this IServiceCollection services, IConfiguration config)
         {
             services.AddScoped<ITokenService, TokenService>();
             services.AddDbContext<DataContext>(options => 
--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -13,10 +13,12 @@ namespace API.Services
     public class TokenService : ITokenService
     {
         private readonly SymmetricSecurityKey _key;
+
         public TokenService(IConfiguration config)
         {
             _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
         }
+        
         public string CreateToken(AppUser user)
         {
             var claims = new List<Claim>
@@ -32,6 +34,7 @@ namespace API.Services
                 Expires = DateTime.Now.AddDays(7),
                 SigningCredentials = creds
             };
+
             var tokenHandler = new JwtSecurityTokenHandler();
 
             var token = tokenHandler.CreateToken(tokenDescriptor);
